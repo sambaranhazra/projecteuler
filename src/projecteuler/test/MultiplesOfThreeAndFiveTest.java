5efaf17c65e452a042bb609f20c7871c5722bf12
sambaran.hazra <sambaran2010@gmail.com>
2016-05-16 08:57:11 +0530

    Initial Commit for https://projecteuler.net/problem=1

---
+++ src/projecteuler/test/MultiplesOfThreeAndFiveTest.java
@@ -0,0 +1,15 @@
+package projecteuler.test;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class MultiplesOfThreeAndFiveTest {
+
+	@Test
+	public void test() {
+		Assert.assertNotNull("Test");
+	}
+
+}
